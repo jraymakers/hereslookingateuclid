d30109da4465a206155db787d4c4b5c29c989e2b
Jeff Raymakers <jephly@gmail.com>
2018-03-24 18:23:01 -0700

    fix typo

--- src/content/book01/Book1Def15to18.tsx
+++ src/content/book01/Book1Def15to18.tsx
@@ -45,7 +45,7 @@ const steps: StepList = [
     name: '17',
     text: [
       ['A ', italic('diameter'), ' of the circle is any straight line drawn through the center ',
-       'and terminated in both directions by the circumference of the circle ',
+       'and terminated in both directions by the circumference of the circle, ',
        'and such a straight line also bisects the circle.'],
     ],
     highlight: [],
