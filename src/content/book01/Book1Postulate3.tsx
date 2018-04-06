03fce0f16fc710adb9c060c8518a3c59a40db2cf
Jeff Raymakers <jephly@gmail.com>
2018-04-05 22:31:34 -0700

    add diagrams for post 3 and 4

--- src/content/book01/Book1Postulate3.tsx
+++ src/content/book01/Book1Postulate3.tsx
@@ -16,22 +16,43 @@ import {
   StepsAndDiagram,
 } from '../../stepsAndDiagram';
 
+import { Greek } from '../Greek';
+
 const width = 400;
 const height = 400;
+
+const aX = width * 0.5;
+const aY = height * 0.5;
+const bX = width * 0.8;
+const bY = aY;
 
 const diagram: Diagram = {
   width,
   height,
-  parts: {},
+  parts: {
+    [Greek.alpha]: { type: 'circle', p1: 'A', p2: 'B', labelDir: LabelDir.W },
+
+    ['AB']: { type: 'line', p1: 'A', p2: 'B' },
+
+    ['A']: { type: 'point', x: aX, y: aY, labelX: -12 },
+    ['B']: { type: 'point', x: bX, y: bY, labelX: 12 },
+  },
 };
 
 const steps: StepList = [
   {
-    name: '3',
+    name: '3a',
     text: [
-      ['Describe a circle with any center and radius.'],
+      ['Let A and B be any two points.'],
     ],
-    highlight: [],
+    highlight: [ 'A', 'B' ],
+  },
+  {
+    name: '3b',
+    text: [
+      [`Then there is a unique circle ${Greek.alpha} with center A and `, italic('radius'), ' AB.'],
+    ],
+    highlight: [ Greek.alpha, 'AB' ],
   },
 ];
 
--- src/content/book01/Book1Postulate4.tsx
+++ src/content/book01/Book1Postulate4.tsx
@@ -19,19 +19,55 @@ import {
 const width = 400;
 const height = 400;
 
+const aX = width * 0.4;
+const aY = height * 0.15;
+const bX = aX;
+const bY = aY + height * 0.2;
+const cX = aX + width * 0.3;
+const cY = bY;
+
+const dX = width * 0.6;
+const dY = height * 0.55;
+const eX = dX;
+const eY = dY + height * 0.3;
+const fX = dX - width * 0.4;
+const fY = eY;
+
 const diagram: Diagram = {
   width,
   height,
-  parts: {},
+  parts: {
+    ['AB']: { type: 'line', p1: 'A', p2: 'B' },
+    ['BC']: { type: 'line', p1: 'B', p2: 'C' },
+
+    ['DE']: { type: 'line', p1: 'D', p2: 'E' },
+    ['EF']: { type: 'line', p1: 'E', p2: 'F' },
+
+    ['A']: { type: 'point', x: aX, y: aY, labelX: -12 },
+    ['B']: { type: 'point', x: bX, y: bY, labelX: -12 },
+    ['C']: { type: 'point', x: cX, y: cY, labelX: 12 },
+
+    ['D']: { type: 'point', x: dX, y: dY, labelX: 12 },
+    ['E']: { type: 'point', x: eX, y: eY, labelX: 12 },
+    ['F']: { type: 'point', x: fX, y: fY, labelX: -12 },
+  },
 };
 
 const steps: StepList = [
   {
-    name: '4',
+    name: '4a',
     text: [
-      ['All right angles equal one another.'],
+      ['Let ABC and DEF be right angles.'],
     ],
-    highlight: [],
+    show: [ 'A', 'B', 'C', 'D', 'E', 'F' ],
+    highlight: [ 'AB', 'BC', 'DE', 'EF' ],
+  },
+  {
+    name: '4b',
+    text: [
+      ['Then the angle ABC equals the angle DEF.'],
+    ],
+    highlight: [ 'AB', 'BC', 'DE', 'EF' ],
   },
 ];
 
