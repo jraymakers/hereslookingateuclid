fdfb76aa83892d4a8d7b29c41e47e74bff0bfcdf
Jeff Raymakers <jephly@gmail.com>
2018-04-05 21:26:47 -0700

    add diagram to def 23

--- src/content/book01/Book1Def23.tsx
+++ src/content/book01/Book1Def23.tsx
@@ -19,10 +19,27 @@ import {
 const width = 400;
 const height = 400;
 
+const aX = 0;
+const aY = height * 0.4;
+const bX = width;
+const bY = height * 0.3;
+const cX = aX;
+const cY = aY + height * 0.3;
+const dX = bX;
+const dY = bY + height * 0.3;
+
 const diagram: Diagram = {
   width,
   height,
-  parts: {},
+  parts: {
+    ['AB']: { type: 'line', p1: 'A', p2: 'B' },
+    ['CD']: { type: 'line', p1: 'C', p2: 'D' },
+
+    ['A']: { type: 'point', x: aX, y: aY },
+    ['B']: { type: 'point', x: bX, y: bY },
+    ['C']: { type: 'point', x: cX, y: cY },
+    ['D']: { type: 'point', x: dX, y: dY },
+  },
 };
 
 const steps: StepList = [
@@ -33,7 +50,7 @@ const steps: StepList = [
        'being in the same plane and being produced indefinitely in both directions, ',
        'do not meet one another in either direction.'],
     ],
-    highlight: [],
+    highlight: [ 'AB', 'CD' ],
   },
 ];
 
